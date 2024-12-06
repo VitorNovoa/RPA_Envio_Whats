6e672bffe055b8bc0fe8dc3f82215944a59ca924
Vitor Novoa <vitor_novoa@hotmail.com>
2024-12-05 22:35:43 -0300

    Atualização de correção

--- RPA_Envio_dados.py
+++ RPA_Envio_dados.py
@@ -1,57 +1,78 @@
 import pywhatkit
 import keyboard
 import time
-from datetime import datetime
-import win32com.client as win32
-from win32com.client import Dispatch
+from datetime import datetime, timedelta
+import win32com.client
 from tkinter import *
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
+import os
+import traceback
+
+arquivo_selecionado = ""
 
 def ler_arquivo():
-    arquivo = filedialog.askopenfilename(initialdir="C:\\RPA\\RPA_Envio_Whats-main", title="Selecione um arquivo", filetypes=(("Planilha do Microsoft Excel", "*.xlsx"),("Planilha do Microsoft Excel","*.xls")))
-    
-    print(arquivo)
-    return arquivo
+    global arquivo_selecionado
+    arquivo_selecionado = filedialog.askopenfilename(
+        initialdir=os.path.dirname(os.path.abspath(__file__)),
+        title="Selecione um arquivo",
+        filetypes=[("Planilha Excel", "*.xlsx"), ("Planilha Excel", "*.xls")]
+    )
+    if arquivo_selecionado:
+        print(f"Arquivo selecionado: {arquivo_selecionado}")
+        messagebox.showinfo("Arquivo Selecionado", "Arquivo carregado com sucesso!")
+    else:
+        print("Nenhum arquivo selecionado")
 
 def envio_mensagem(arquivo):
     try:
-        xls = Dispatch('Excel.Application')
+        if not arquivo:
+            messagebox.showwarning("Aviso", "Selecione um arquivo primeiro!")
+            return
+
+        xls = win32com.client.Dispatch("Excel.Application")
         xls.Visible = False
         xls.DisplayAlerts = False
-        
-        xls.Workbooks.Open(arquivo)  
 
-        xls.Worksheets('Planilha1')
-            
+        workbook = xls.Workbooks.Open(arquivo)
+        sheet = workbook.Worksheets("Planilha1")
+
         x = 3
-        cell = xls.Range("A" + str(x)).Value
+        cell = sheet.Range("A" + str(x)).Value
 
-        while cell != None: 
+        while cell is not None:
+            aluno = sheet.Range("A" + str(x)).Value
+            valor = sheet.Range("E" + str(x)).Value
+            numero = sheet.Range("F" + str(x)).Value
 
-            aluno = xls.Range("A" +str(x)).Value
-            valor = xls.Range("E" + str(x)).Value
-            numero = xls.Range("F"+ str(x)).Value
+            mensagem = f"""Olá responsável pelo/a: {aluno}, gostaria das seguintes informações para o próximo ano letivo 2023:
+            Ressaltando o valor de R$ {valor}, para 2025.
+            Desejo continuar na van
+            Não desejo continuar na van
+            Período letivo do/a: {aluno} caso deseje continuar.
+            Manhã
+            Tarde
+            Integral
+            """
 
-            mensagem = "Olá responsável pelo/a: " + aluno + ", gostaria das seguintes informações para o próximo ano letivo 2023:" + "\n" + "Ressaltando o valor de R$" + str(valor) + ", para 2023." + "\n" + "Desejo continuar na van" + "\n" + "Não desejo continuar na van" + "\n" + "Período letivo do/a: " + aluno + " caso deseje continuar." + "\n" + "Manhã" + "\n" + "Tarde" + "\n" + "Integral" + "\n"
-        
-            pywhatkit.sendwhatmsg(numero, mensagem, datetime.now().hour, datetime.now().minute + 2)
+            horario = datetime.now()
+            pywhatkit.sendwhatmsg(numero, mensagem, horario.hour, horario.minute + 1)
             time.sleep(2)
-            keyboard.press_and_release('ctrl + w')
-        
-            x += 1 
-            cell = xls.Range("A" + str(x)).Value
+            keyboard.press_and_release("ctrl + w")
 
-        xls.ActiveWorkbook.Save()
-        xls.Application.Quit()
+            x += 1
+            cell = sheet.Range("A" + str(x)).Value
 
+        workbook.Save()
+        xls.Quit()
+        messagebox.showinfo("Sucesso", "Mensagens enviadas com sucesso!")
 
-    except Exception as ex:
-        print(ex.args[0])
+    except Exception as e:
+        print(traceback.format_exc())
+        messagebox.showerror("Erro", f"Ocorreu um erro: {e}")
 
 janela = Tk()
-
 janela.title("Envio de mensagens para os pais")
-janela.geometry("450x130")
+janela.geometry("450x150")
 
 lbl_mensagem_excel = Label(janela, text="Selecione o arquivo desejado")
 lbl_mensagem_excel.grid(column=0, row=0, padx=10, pady=10)
@@ -62,7 +83,7 @@ btn_excel.grid(column=1, row=0, padx=10, pady=10)
 lbl_mensagem = Label(janela, text="Clique no botão abaixo para executar a tarefa")
 lbl_mensagem.grid(column=0, row=1, padx=10, pady=10)
 
-btn_executar = Button(janela, text="Executar", command= lambda: envio_mensagem(btn_excel))
+btn_executar = Button(janela, text="Executar", command=lambda: envio_mensagem(arquivo_selecionado))
 btn_executar.grid(column=1, row=1, padx=10, pady=10)
 
 janela.mainloop()
